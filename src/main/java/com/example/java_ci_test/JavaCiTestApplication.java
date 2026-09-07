e08b79f795ffec58d42ae2e70c525d2aeb7bef56
이정준 <ljj10571@gmail.com>
2026-09-07 15:49:59 +0900

    init

---
+++ src/main/java/com/example/java_ci_test/JavaCiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_ci_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_ci_test/JavaCiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_ci_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
